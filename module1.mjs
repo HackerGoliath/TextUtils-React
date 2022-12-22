207f3400ab50f9f62322cabce903a1ac7aa7ae03
Deepak <deepaksingh435@gmail.com>
2022-12-22 17:20:51 +0530

    6.Added Props

---
+++ module1.mjs
@@ -0,0 +1,9 @@
+// Object export by default can be have any name in import
+// for eg: ui can be dz or any other name
+// But a,b, and c can not have different name and enclosed within culrly braces
+import ui, { a, b, c } from './module2.mjs';
+
+console.log(ui)
+console.log(a)
+console.log(b)
+
---
+++ module2.mjs
@@ -0,0 +1,12 @@
+const a = "Deepak";
+const b = "Rohan";
+const c = "Rohit";
+const d = "Priyanka";
+
+
+// export default [d, c];
+export default d;
+// Named export
+export { a }
+export { b }
+export { c }

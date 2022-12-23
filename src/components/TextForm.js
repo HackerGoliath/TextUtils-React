9f5e88b89166f855431a8b1147a454e1a51a4c1d
Deepak <deepaksingh435@gmail.com>
2022-12-23 18:04:34 +0530

    Added Extra Buttons

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -14,6 +14,24 @@ export default function TextForm(props) {
         setText(newText)
     }
 
+    const handleCopy = () => {
+        // console.log("Uppercase was clicked: " + text)
+        var text = document.getElementById('myBox')
+        text.select();
+        navigator.clipboard.writeText(text.value)
+    }
+
+    const handleExtraSpaces = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "))
+    }
+
+    const handleClearClick = () => {
+        // console.log("Uppercase was clicked: " + text)
+        let newText = ''
+        setText(newText)
+    }
+
     const handleOnChange = (event) => {
         // console.log("On Change was clicked")
         setText(event.target.value)
@@ -31,6 +49,9 @@ export default function TextForm(props) {
                 </div>
                 <button className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
                 <button className="btn btn-primary mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button className="btn btn-primary mx-1" onClick={handleCopy}>Copy Text</button>
+                <button className="btn btn-primary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
+                <button className="btn btn-primary mx-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className="container my-3">
                 <h2>Your text summary</h2>

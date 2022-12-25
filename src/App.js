33bd29615d74cce2860dd09b5eba23da935a7af5
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2022-12-25 18:26:34 +0530

    21 Fixed issues

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React, { useState } from 'react'
 import './App.css';
 import Alert from './components/Alert';
-// import { BrowserRouter, Routes, Route } from 'react-router-dom';
-// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
-// import {
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   Route,
-//   RouterProvider,
-// } from "react-router-dom";
-// import {
-//   BrowserRouter as Router,
-//   Switch,
-//   Route,
-//   // Link
-// } from "react-router-dom";
+
 
 function App() {
   const [mode, setMode] = useState('light');
@@ -36,35 +23,14 @@ function App() {
       setMode('dark');
       document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode enabled", "success");
-      // document.title = "TextUtils - Home(Dark Mode)";
-      // setInterval(() => {
-      //   document.title = "TextUtils is amazing";
-      // }, 2000);
-      // setInterval(() => {
-      //   document.title = "Install TextUtils now";
-      // }, 1500);
     }
     else {
       setMode('light');
       document.body.style.backgroundColor = "whitesmoke"
       showAlert("Light mode enabled", "success");
-      // document.title = "TextUtils - Home(Light Mode)";
     }
   }
   return (
-    // <BrowserRouter>
-
-    //   <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-    //   <Alert alert={alert} />
-    //   <div className="container my-3">
-    //     {/* <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} /> */}
-    //     <Routes>
-    //       <Route path='/' element={<TextForm showAlert={showAlert} heading="Try TextUtils - Word Counter, Character Counter, Remove Extra Spaces" mode={mode} />} />
-    //       <Route path='/about' element={<About mode={mode} />} />
-    //     </Routes>
-    //   </div>
-    // </BrowserRouter>
-
     <>
       <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
       <Alert alert={alert} />
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -3,21 +3,18 @@ import React, { useState } from 'react'
 
 export default function TextForm(props) {
     const handleUpClick = () => {
-        // console.log("Uppercase was clicked: " + text)
         let newText = text.toUpperCase()
         setText(newText)
         props.showAlert("Converted to uppercase", "success");
     }
 
     const handleLoClick = () => {
-        // console.log("Uppercase was clicked: " + text)
         let newText = text.toLowerCase()
         setText(newText)
         props.showAlert("Converted to lowercase", "success");
     }
 
     const handleCopy = () => {
-        // console.log("Uppercase was clicked: " + text)
         var text = document.getElementById('myBox')
         text.select();
         navigator.clipboard.writeText(text.value);
@@ -32,14 +29,12 @@ export default function TextForm(props) {
     }
 
     const handleClearClick = () => {
-        // console.log("Uppercase was clicked: " + text)
         let newText = ''
         setText(newText)
         props.showAlert("Text cleared", "success");
     }
 
     const handleOnChange = (event) => {
-        // console.log("On Change was clicked")
         setText(event.target.value)
     }
 
@@ -61,7 +56,7 @@ export default function TextForm(props) {
             </div>
             <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
                 <h2>Your text summary</h2>
-                <p>{text.split(" ").filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
+                <p>{text.split(/\s+/).filter((element) => { return element.length !== 0 }).length} words and {text.length} characters</p>
                 <p>{0.008 * text.split(" ").filter((element) => { return element.length !== 0 }).length} minutes to read</p>
                 <h2>Preview</h2>
                 <p>{text.length > 0 ? text : "Nothing to preview!"}</p>

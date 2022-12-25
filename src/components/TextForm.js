0f0588a1dcf70a19c5ff0bcb485539dacae18907
HackerGoliath <111438818+HackerGoliath@users.noreply.github.com>
2022-12-25 18:45:53 +0530

    21 wrapped

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -15,10 +15,7 @@ export default function TextForm(props) {
     }
 
     const handleCopy = () => {
-        var text = document.getElementById('myBox')
-        text.select();
-        navigator.clipboard.writeText(text.value);
-        document.getSelection().removeAllRanges();
+        navigator.clipboard.writeText(text);
         props.showAlert("Text copied to clipboard", "success");
     }
 
